42fb64e8683c6d21617ba7870fa2723cc8849659
oyeitsmeow <zhongzichenn@gmail.com>
2024-09-10 02:12:55 +0800

    chore: Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,10 @@
 PM2_APP_NAME=ts2mock-node-server
 PORT=3000
 
-sudo pm2 stop ${PM2_APP_NAME}
+pm2 stop ${PM2_APP_NAME}
 
-sudo pm2 delete ${PM2_APP_NAME}
+pm2 delete ${PM2_APP_NAME}
 
-sudo PORT=${PORT} pm2 start /var/www/packages/ts2mock-node-setup/app.js --name ${PM2_APP_NAME}
+PORT=${PORT} pm2 start /var/www/packages/ts2mock-node-setup/dist/app.js --name ${PM2_APP_NAME}
 
-rm /var/www/packages/ts2mock-node/package.tgz
-
-# 输出 pm2 状态
-sudo pm2 list
+# rm /var/www/packages/ts2mock-node/package.tgz
