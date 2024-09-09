73c47b99da66edbedc9f61122837146af511ab9b
oyeitsmeow <zhongzichenn@gmail.com>
2024-09-10 02:39:07 +0800

    chore: Update NODE_PATH

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,13 @@
 
 PM2_APP_NAME=PM2_APP_NAME
 PORT=PORT
+NODE_PATH=NODE_PATH
 
 pm2 stop ${PM2_APP_NAME}
 
 pm2 delete ${PM2_APP_NAME}
 
-export PATH=/root/.nvm/versions/node/v18.20.4/bin:$PATH
+export PATH=NODE_PATH:$PATH
 
 PORT=${PORT} pm2 start /var/www/packages/ts2mock-node-setup/dist/app.js --name ${PM2_APP_NAME}
 
